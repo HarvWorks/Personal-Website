a9f022ee33a016ca0f499356806f92f96b734b0c
Harvey Chui <chuiharvey@gmail.com>
2017-02-12 04:15:29 -0800

    First draft is finished

--- client/js/routes.js
+++ client/js/routes.js
@@ -1,23 +1,13 @@
 var app = angular.module('app', ['ngRoute']);
-app.config(function($routeProvider) {
+
+app.config(function($routeProvider, $locationProvider) {
   $routeProvider
     .when('/', {
       templateUrl: './partials/index.html',
       controller: 'indexController'
     })
-    .when('/dashboard', {
-      templateUrl: './partials/portfolio.html',
-      controller: 'dashboardController',
-    })
-    .when('/poll/:id', {
-      templateUrl: './partials/show.html',
-      controller: 'showController',
-    })
-    .when('/create', {
-      templateUrl: './partials/topic.html',
-      controller: 'topicController',
-    })
     .otherwise({
       redirectTo: '/'
     });
+
 });
